a129edb673f0e8e5d34f037692a89b278e5a2055
railken <railken@protonmail.ch>
2019-01-21 12:50:03 +0100

    cleanup

--- config/amethyst.project.http.admin.project.php
+++ config/amethyst.project.http.admin.project.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'enabled'     => true,
-    'controller'  => Railken\Amethyst\Http\Controllers\Admin\ProjectsController::class,
-    'router'      => [
-        'as'        => 'project.',
-        'prefix'    => '/projects',
+    'enabled'    => true,
+    'controller' => Railken\Amethyst\Http\Controllers\Admin\ProjectsController::class,
+    'router'     => [
+        'as'     => 'project.',
+        'prefix' => '/projects',
     ],
 ];
