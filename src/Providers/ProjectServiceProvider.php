f5b9dc57ea66886aa45a9e516af9675a59165607
railken <railken@protonmail.ch>
2019-02-15 16:40:23 +0100

    update taxonomy

--- src/Providers/ProjectServiceProvider.php
+++ src/Providers/ProjectServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Railken\Amethyst\Providers;
 
+use Illuminate\Support\Facades\Config;
 use Railken\Amethyst\Common\CommonServiceProvider;
 
 class ProjectServiceProvider extends CommonServiceProvider
@@ -13,5 +14,7 @@ class ProjectServiceProvider extends CommonServiceProvider
     {
         parent::register();
         $this->app->register(\Railken\Amethyst\Providers\TaxonomyServiceProvider::class);
+
+        Config::push('amethyst.taxonomy.data.taxonomy.seeds', ['name' => Config::get('amethyst.project.data.project.attributes.status.vocabulary')]);
     }
 }
