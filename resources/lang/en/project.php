4a7562abfbd99fe677b72616fbe1cc8d2ce1a645
railken <railken@protonmail.ch>
2019-04-06 22:42:45 +0200

    add lang

---
+++ resources/lang/en/project.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'name' => 'Project',
+    'description' => 'Project',
+    'attributes' => [
+        'status_id' => [
+            'label' => 'Status'
+        ],
+        'value' => [
+            'label' => 'Value',
+            'description' => 'A generic value appliable to the project (e.g. Project Cost, Project Sellable Value)'
+        ],
+        'tag' => [
+            'label' => 'Tags'
+        ]
+    ]
+]
---
+++ resources/lang/it/project.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'name' => 'Project',
+    'description' => 'Project',
+    'attributes' => [
+        'status_id' => [
+            'label' => 'Stato'
+        ],
+        'value' => [
+            'label' => 'Valore',
+            'description' => 'Un generico valore applicabile al progetto (e.g. Costo del Progetto, Valore vendibile del Progetto)'
+        ],
+        'tag' => [
+            'label' => 'Tags'
+        ]
+    ]
+]
