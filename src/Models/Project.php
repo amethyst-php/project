8aeb34e9344051cd43c3ec5d114de40f4a0c121e
railken <railken@protonmail.ch>
2019-05-11 16:54:53 +0200

    fix relationships

--- src/Models/Project.php
+++ src/Models/Project.php
@@ -29,7 +29,7 @@ class Project extends Model implements EntityContract
      */
     public function status(): BelongsTo
     {
-        return $this->belongsTo(Taxonomy::class);
+        return $this->belongsTo(config('amethyst.taxonomy.data.taxonomy.model'));
     }
 
     /**
