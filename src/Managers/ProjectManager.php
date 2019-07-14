2bca8262a19d319cad84b5d3516ed06a44694d43
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/ProjectManager.php
+++ src/Managers/ProjectManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\Project newEntity()
+ * @method \Amethyst\Schemas\ProjectSchema getSchema()
+ * @method \Amethyst\Repositories\ProjectRepository getRepository()
+ * @method \Amethyst\Serializers\ProjectSerializer getSerializer()
+ * @method \Amethyst\Validators\ProjectValidator getValidator()
+ * @method \Amethyst\Authorizers\ProjectAuthorizer getAuthorizer()
+ */
 class ProjectManager extends Manager
 {
     use ConfigurableManager;
