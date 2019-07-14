cfbd67821603b182015ddda2a30fe6c1848dec00
railken <railken@protonmail.ch>
2019-07-14 13:11:15 +0200

    fix style

--- src/Managers/ProjectManager.php
+++ src/Managers/ProjectManager.php
@@ -6,12 +6,12 @@ use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
 /**
- * @method \Amethyst\Models\Project newEntity()
- * @method \Amethyst\Schemas\ProjectSchema getSchema()
+ * @method \Amethyst\Models\Project                 newEntity()
+ * @method \Amethyst\Schemas\ProjectSchema          getSchema()
  * @method \Amethyst\Repositories\ProjectRepository getRepository()
- * @method \Amethyst\Serializers\ProjectSerializer getSerializer()
- * @method \Amethyst\Validators\ProjectValidator getValidator()
- * @method \Amethyst\Authorizers\ProjectAuthorizer getAuthorizer()
+ * @method \Amethyst\Serializers\ProjectSerializer  getSerializer()
+ * @method \Amethyst\Validators\ProjectValidator    getValidator()
+ * @method \Amethyst\Authorizers\ProjectAuthorizer  getAuthorizer()
  */
 class ProjectManager extends Manager
 {
